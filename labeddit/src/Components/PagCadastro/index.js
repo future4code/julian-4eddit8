34b690517d3fef6c8983cc33c8a7ad85a5efa980
Caio Mantovani <c.saadmantovani@gmail.com>
2020-06-08 15:02:25 -0300

    componentes cadastro e post sem estilizacao

---
+++ labeddit/src/Components/PagCadastro/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function PagCadastro() {
+    return (
+      <div>
+        <h3>Cadastro novo usuário</h3>
+        <input placeholder="Nome de usuário"></input> <br/>
+        <input placeholder="Email"></input> <br/>
+        <input placeholder="Senha"></input><br/>
+         <button>Cadastrar</button>
+      </div>
+    );
+  }
+  
+  export default PagCadastro;
