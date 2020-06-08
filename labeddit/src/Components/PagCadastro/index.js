93ca96e7084b2a52c7f6fea37dae54e8fdabc5b9
Caio Mantovani <c.saadmantovani@gmail.com>
2020-06-08 16:19:02 -0300

    Pag cadastro finalizada

--- labeddit/src/Components/PagCadastro/index.js
+++ labeddit/src/Components/PagCadastro/index.js
@@ -1,15 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+//display flex, justify
+
+const ContainerCadastro = styled.div`
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
 
 
 function PagCadastro() {
     return (
-      <div>
+      <ContainerCadastro>
         <h3>Cadastro novo usuário</h3>
-        <input placeholder="Nome de usuário"></input> <br/>
-        <input placeholder="Email"></input> <br/>
-        <input placeholder="Senha"></input><br/>
-         <button>Cadastrar</button>
-      </div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nome de usuário"
+          defaultValue=""
+          variant="outlined"
+        />
+
+        <br/>
+
+         <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          defaultValue=""
+          variant="outlined"
+        />
+
+        <br/>
+       
+        <TextField
+            required
+            id="outlined-password-input"
+            label="Senha"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+
+        <br/>
+        
+        <Button variant="contained" color="primary" disableElevation>Cadastrar</Button>
+        
+    
+      </ContainerCadastro>
     );
   }
   
